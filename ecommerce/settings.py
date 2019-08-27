ad83619379497a97d418959ae4aea23cce793133
Ubuntu <ubuntu@ip-172-31-94-120.ec2.internal>
2019-08-27 14:45:21 +0000

    Imported accounts app and configured settings.py file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,11 +24,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# -----------------------------EDITED-----------------------------
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# -----------------------------EDITED-----------------------------
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
 
 
@@ -41,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # -----------------------------ADDED-----------------------------
+    'django_forms_bootstrap',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +63,9 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # -----------------------------EDITED-----------------------------
+        # Specify that any dir called templates could contain templates
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,6 +110,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# -----------------------------ADDED-----------------------------
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -122,3 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# -----------------------------ADDED-----------------------------
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
