6cfeb90bab7e729c8ea1dd37846f9bb49a232833
Ubuntu <ubuntu@ip-172-31-94-120.ec2.internal>
2019-08-31 12:43:43 +0000

    Removed env.py from GitHub

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault("HOSTNAME","ecommerce-hebs87.herokuapp.com")
-os.environ.setdefault("SECRET_KEY", "'sf6_#*anu2k-!2w00**3%xans4pum-@u!1#7ntr77(5!%1!&_%'")
-os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_3w7KJsWAPMzX0XoPkunOM7N900NOUBJDTw")
-os.environ.setdefault("STRIPE_SECRET", "sk_test_9Z4jISqoMBRY7ytwiULmvSbU00cYvNvYNc")
-os.environ.setdefault("DATABASE_URL", "postgres://rfyagzqmjtfqbn:64c2b08c1964d63a4a6eb6a500a78106c33daed1b561111496efd5ce2af4b5c6@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/d6jumr3mgst7t0")
-os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIASNSR7AO4YVCAU5G5")
-os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
